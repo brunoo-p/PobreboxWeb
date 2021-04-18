87ec07f234a243a690c3c1b059dd95298c9f1c7f
bruno <brun.op@hotmail.com>
2021-04-17 23:30:18 -0300

    Refactoring

--- pobrebox.ui/src/Components/DirectoryCard/index.jsx
+++ pobrebox.ui/src/Components/DirectoryCard/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import { Wrapper, Content, DirectoryLogo } from './styles';
 
-export default function DirectoryCard({children, openModal, items, setDirectory}) {
+export default function DirectoryCard({ items, openModal, scrollDir }) {
     
     return (
         <Wrapper >
-             {children}
+            <div className="contentDirectory" style={{width: items.length * 350, marginLeft: scrollDir}}>
+                    
+                { items && items.map((item, index) => {
+                    
+                    return(
+                        <ol key={index} >
+                            <li >
+                                <Content onClick={() => openModal(item.text)} >
+                                    <h3><DirectoryLogo/></h3>
+                                    <span>{item.text}</span>
+                                    <p>click para ver os arquivos salvos</p>
+                                </Content>
+                            </li>
+                        </ol>
+                    )})
+                }
+                
+            </div>
         </Wrapper>
     )
 }
--- pobrebox.ui/src/Components/DirectoryCard/styles.js
+++ pobrebox.ui/src/Components/DirectoryCard/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import {GoFileDirectory} from 'react-icons/go';
 
 export const Wrapper = styled.div`
     display: flex;
@@ -26,4 +26,92 @@ export const Wrapper = styled.div`
         }
     }
     
+`;
+
+export const Content = styled.div`
+
+    display: inline-block;
+    max-width: 110px;
+    width: 100%;
+    height: 80px;
+    transform: scale(0.95);
+    transition: 0.2s ease-in;
+    margin: 20px;
+    perspective: 1200px;
+    
+    padding: 20px;
+    background: #fff;
+    
+    
+    border-radius: 20px;
+    border: 2px solid transparent;
+    cursor: pointer;
+    
+    h3{
+        display: flex;
+        margin-top: -5px;
+        justify-content: center;
+    }
+    span{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 2px;
+    }
+    p{ 
+        display: flex;
+        justify-content: center;
+    }
+
+    :hover{
+        transform: scale(1);
+        transition: 0.2s ease-in
+    }
+
+
+    @media (min-width: 1024px){
+        max-width: 200px;
+        padding: 20px;
+        margin: 0px;
+    }
+
+    @media (max-width: 550px){
+        max-width: 70px;
+        padding: 10px;
+        
+        p{display: none;}
+    }
+
+    
+
+    span{
+        padding: 4px;
+        font-weight: 900;
+        font-size: 12px;
+    }
+    p{
+        margin-top: 5px;
+        font-size: 9px;
+        color: ${({theme}) => theme.font.color};
+    }
+
+    
+
+    @media (min-width: 1024px){
+
+        span{
+            font-size: 16px;
+        }
+        p{
+            font-size: 10px;
+        }
+    }
+`;
+
+export const DirectoryLogo = styled(GoFileDirectory)`
+    width: 30px;
+    height: 28px;
+    fill: ${({ theme }) => theme.colors.primaryColor};
+    border-radius: 50%;
+    padding: 5px;
+    background: rgba(116, 185, 255,.6);
 `;
